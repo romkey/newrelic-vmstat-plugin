e171e9e8f43e5959634fe18fe414587f61942a2a
John Romkey <romkey@romkey.com>
2016-02-01 01:15:59 -0500

    config file is .yml
    fix grabbing the metrics
    
    fix guid
    
    pass empty units parameter

--- vmstat_agent.rb
+++ vmstat_agent.rb
@@ -2,22 +2,24 @@
 
 # records vmstat output on New Relic
 
-#require "rubygems"
-#require "bundler/setup"
-require "newrelic_plugin"
+require 'newrelic_plugin'
 
 module VmstatAgent
   class Agent < NewRelic::Plugin::Agent::Base
-    agent_guid "com.romkey.newrelic.memory"
+    agent_guid "com.romkey.newrelic.vmstat"
     agent_human_labels("Platform") { `hostname` }
     agent_version '0.0.1'
+
+    def setup_metrics
+      
+    end
 
     def poll_cycle
       stats = memory_stats
 
       if stats
         stats.each do |key,value|
-          report_metric key, value
+          report_metric key, '', value
         end
       end
     end
@@ -29,13 +31,14 @@ module VmstatAgent
 
       vmstats.each do |stat|
         results = stat.match /(\d+) (K )*(.+)/
-        info[results[3]] = info[results[1]
+        info[results[3]] = results[1]
       end
 
       return info
     end
 
-    NewRelic::Plugin::Config.config_file = "config/memory_config.yml"
+    NewRelic::Plugin::Config.config_file = "config/vmstat_config.yml"
     NewRelic::Plugin::Setup.install_agent :vmstat, VmstatAgent
     NewRelic::Plugin::Run.setup_and_run
+  end
 end
